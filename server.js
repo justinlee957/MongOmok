b09440b575d3e2a2585ac40835f5dc925446e145
Justin Lee <jlee7605@usc.edu>
2021-01-15 11:57:26 -0800

    update

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ app.use(cors())
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
+        origin: "*",
       }
 })
 
