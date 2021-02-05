94a992f895895740417fcae93ab0f945a455bbb5
Justin Lee <jlee7605@usc.edu>
2021-02-04 17:21:41 -0800

    update

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -92,8 +92,8 @@ class Layout extends Component{
     componentDidMount(){
       let isMounted = true
       //used for server
-      //this.socket = openSocket({query: `uid=${this.props.uid}`})
-      this.socket = openSocket("http://localhost:5000",{query: `uid=${this.props.uid}`})
+      this.socket = openSocket({query: `uid=${this.props.uid}`})
+      //this.socket = openSocket("http://localhost:5000",{query: `uid=${this.props.uid}`})
       this.socket.on('startGame', (data) =>{
           if(isMounted){
             this.setState({gameData: data})
